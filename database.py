fba05ce0402d03c6f4f3b56d593fedb1603ee819
ishita2004 <ishitax2004@gmail.com>
2025-09-08 21:57:13 +0530

    Remove unnecessary database dependencies

--- database.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-DATABASE_URL = "postgresql://postgres:ishita123@localhost/smartbiziq"
-
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
