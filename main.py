06fc24e6e730173fec67dfe9bfc849d238fd954f
ishita2004 <ishitax2004@gmail.com>
2025-09-09 01:49:19 +0530

    Update server.js: Added CORS for deployed frontend

--- main.py
+++ main.py
@@ -38,9 +38,8 @@ app = FastAPI()
 # Enable CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[ 
-        "https://smart-biz-iq-frontend1.vercel.app"
-    ],
+    allow_origins=[
+        "http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
